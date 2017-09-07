086b3760be342541a3936497fbda9ebaffa742b9
Meagan Lang <langmm.astro@gmail.com>
2017-09-07 01:56:43 -0500

    Instead just pass error message, no exception

--- cis_interface/drivers/tests/test_RMQClientDriver.py
+++ cis_interface/drivers/tests/test_RMQClientDriver.py
@@ -33,9 +33,11 @@ class TestRMQClientParam(parent1.TestRMQParam, IOInfo):
                            '_deliveries', '_acked', '_nacked',
                            '_message_number']
         self._temp_queue = 'TestRMQClientDriver_SERVER'
-        if getattr(self, '_channel', None):
-            if self.channel.is_open:
+        if getattr(self, 'channel', None):
+            try:
                 self.channel.queue_purge(queue=self.temp_queue)
+            except:
+                self.instance.error("Could not purge test queue.")
             
 
 class TestRMQClientDriverNoStart(TestRMQClientParam,
