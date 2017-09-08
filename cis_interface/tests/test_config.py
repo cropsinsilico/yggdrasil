c878f4e01139c7a6ab084801a945f5c1ce6f39c5
Meagan Lang <langmm.astro@gmail.com>
2017-09-08 14:46:26 -0500

    Test of config

---
+++ cis_interface/tests/test_config.py
@@ -0,0 +1,16 @@
+import nose.tools as nt
+from cis_interface import config
+
+
+def test_CisConfigParser():
+    r"""Ensure that get returns proper defaults etc."""
+    x = config.CisConfigParser()
+    x.add_section('test_section')
+    x.set('test_section', 'test_option', 'test_value')
+    nt.assert_equal(x.get('test_section', 'test_option'), 'test_value')
+    nt.assert_equal(x.get('test_section', 'fake_option'), None)
+    nt.assert_equal(x.get('test_section', 'fake_option', 5), 5)
+    nt.assert_equal(x.get('fake_section', 'fake_option'), None)
+    nt.assert_equal(x.get('fake_section', 'fake_option', 5), 5)
+
+
