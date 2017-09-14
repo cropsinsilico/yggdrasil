0cbdc2b85a0ea32e6e539bfc8d6c05ed5c9622c0
Meagan Lang <langmm.astro@gmail.com>
2017-09-13 21:17:23 -0500

    Change regex_replace

--- cis_interface/interface/PsiInterface.hpp
+++ cis_interface/interface/PsiInterface.hpp
@@ -475,7 +475,7 @@ public:
     std::regex e("%(?:\\d+\\$)?[+-]?(?:[ 0]|'.{1})?-?\\d*(?:\\.\\d+)?(?:[lhjztL])*([eEfFgG])");
     std::string s(_pi._psi._fmt, strlen(_pi._psi._fmt));
     std::string result;
-    std::regex_replace(std::back_inserter(result), s.begin(), s.end(), e, "%$1");
+    result = std::regex_replace(s, e, "%$1");
     strcpy(_pi._psi._fmt, result.c_str());
   }
   /*!
