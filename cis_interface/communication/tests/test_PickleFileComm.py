a83ab3ead0f03e4ccc7c58e4f6334f433d7e58fb
Meagan Lang <langmm.astro@gmail.com>
2017-10-10 21:20:04 -0500

    pickle input comm return string and add driver

--- cis_interface/communication/tests/test_PickleFileComm.py
+++ cis_interface/communication/tests/test_PickleFileComm.py
@@ -1,4 +1,3 @@
-import nose.tools as nt
 from cis_interface.communication.tests import test_FileComm as parent
 
 
@@ -7,29 +6,3 @@ class TestPickleFileComm(parent.TestFileComm):
     def __init__(self, *args, **kwargs):
         super(TestPickleFileComm, self).__init__(*args, **kwargs)
         self.comm = 'PickleFileComm'
-
-    def test_send_recv(self):
-        r"""Test send/recv of a small message."""
-        nt.assert_equal(self.send_instance.n_msg, 0)
-        nt.assert_equal(self.recv_instance.n_msg, 0)
-        flag = self.send_instance.send(self.data_dict)
-        assert(flag)
-        nt.assert_equal(self.recv_instance.n_msg, 1)
-        flag, msg_recv = self.recv_instance.recv()
-        assert(flag)
-        self.assert_equal_data_dict(msg_recv)
-        nt.assert_equal(self.send_instance.n_msg, 0)
-        nt.assert_equal(self.recv_instance.n_msg, 0)
-        
-    def test_send_recv_nolimit(self):
-        r"""Test send/recv of a large message."""
-        nt.assert_equal(self.send_instance.n_msg, 0)
-        nt.assert_equal(self.recv_instance.n_msg, 0)
-        flag = self.send_instance.send_nolimit(self.data_dict)
-        assert(flag)
-        assert(self.recv_instance.n_msg >= 1)
-        flag, msg_recv = self.recv_instance.recv_nolimit()
-        assert(flag)
-        self.assert_equal_data_dict(msg_recv)
-        nt.assert_equal(self.send_instance.n_msg, 0)
-        nt.assert_equal(self.recv_instance.n_msg, 0)
