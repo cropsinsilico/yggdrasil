e70eaf23af0fbdc6e9df90edb2c9fc626e08df19
Meagan Lang <langmm.astro@gmail.com>
2017-10-14 19:01:07 -0500

    Fix issue with providing keywords to rpc comm

--- cis_interface/communication/RPCComm.py
+++ cis_interface/communication/RPCComm.py
@@ -1,5 +1,5 @@
 import os
-from cis_interface.communication import DefaultComm, CommBase
+from cis_interface.communication import CommBase, get_comm, get_comm_class
 
 
 _rpc_address_split = '_FROM_RPC_TO_'
@@ -18,8 +18,8 @@ class RPCComm(CommBase.CommBase):
         **kwargs: Additional keywords arguments are passed to parent class.
 
     Attributes:
-        icomm (DefaultComm): Input comm.
-        ocomm (DefaultComm): Output comm.
+        icomm (Comm): Input comm.
+        ocomm (Comm): Output comm.
 
     """
     def __init__(self, name, icomm_kwargs=None, ocomm_kwargs=None,
@@ -40,11 +40,11 @@ class RPCComm(CommBase.CommBase):
                 'address', self.address.split(_rpc_address_split)[0])
             ocomm_kwargs.setdefault(
                 'address', self.address.split(_rpc_address_split)[1])
-            self.icomm = DefaultComm(icomm_name, **icomm_kwargs)
-            self.ocomm = DefaultComm(ocomm_name, **ocomm_kwargs)
+            self.icomm = get_comm(icomm_name, **icomm_kwargs)
+            self.ocomm = get_comm(ocomm_name, **ocomm_kwargs)
         else:
-            self.icomm = DefaultComm(icomm_name, **icomm_kwargs)
-            self.ocomm = DefaultComm(ocomm_name, **ocomm_kwargs)
+            self.icomm = get_comm(icomm_name, **icomm_kwargs)
+            self.ocomm = get_comm(ocomm_name, **ocomm_kwargs)
             kwargs.setdefault('address', _rpc_address_split.join(
                 [self.icomm.address, self.ocomm.address]))
             super(RPCComm, self).__init__(name, dont_open=True, **kwargs)
@@ -57,12 +57,68 @@ class RPCComm(CommBase.CommBase):
         return self.icomm.comm_count
 
     @classmethod
-    def new_comm_kwargs(cls, *args, **kwargs):
-        r"""Initialize communication with new queue."""
-        iargs, ikwargs = DefaultComm.new_comm_kwargs(*args, **kwargs)
-        oargs, okwargs = DefaultComm.new_comm_kwargs(*args, **kwargs)
-        kwargs.setdefault('address', _rpc_address_split.join([
-            ikwargs['address'], okwargs['address']]))
+    def new_comm_kwargs(cls, name, icomm_name=None, ocomm_name=None,
+                        icomm_comm=None, ocomm_comm=None,
+                        icomm_kwargs=None, ocomm_kwargs=None, **kwargs):
+        r"""Initialize communication with new comms.
+
+        Args:
+            name (str): Name for new comm.
+            icomm_name (str, optional): Name for new input comm. Defaults to
+                name + '_IN'. This will be overriden if 'name' is in
+                icomm_kwargs.
+            ocomm_name (str, optional): Name for new output comm. Defaults to
+                name + '_OUT'. This will be overriden if 'name' is in
+                ocomm_kwargs.
+            icomm_comm (str, optional): Name of class for new input comm.
+                Defaults to None. This will be overrriden if 'comm' is in
+                icomm_kwargs.
+            ocomm_comm (str, optional): Name of class for new output comm.
+                Defaults to None. This will be overrriden if 'comm' is in
+                ocomm_kwargs.
+            icomm_kwargs (dict, optional): Keyword arguments for the icomm_comm
+                new_comm_kwargs class method.
+            ocomm_kwargs (dict, optional): Keyword arguments for the ocomm_comm
+                new_comm_kwargs class method.
+
+        """
+        args = [name]
+        if icomm_kwargs is None:
+            icomm_kwargs = dict()
+        if ocomm_kwargs is None:
+            ocomm_kwargs = dict()
+        if icomm_name is None:
+            icomm_name = name + '_IN'
+        if ocomm_name is None:
+            ocomm_name = name + '_OUT'
+        icomm_name = icomm_kwargs.pop('name', icomm_name)
+        ocomm_name = ocomm_kwargs.pop('name', ocomm_name)
+        icomm_comm = icomm_kwargs.pop('comm', icomm_comm)
+        ocomm_comm = ocomm_kwargs.pop('comm', ocomm_comm)
+        icomm_class = get_comm_class(icomm_comm)
+        ocomm_class = get_comm_class(ocomm_comm)
+        icomm_kwargs['direction'] = 'recv'
+        ocomm_kwargs['direction'] = 'send'
+        ikwargs = dict(**icomm_kwargs)
+        okwargs = dict(**ocomm_kwargs)
+        if 'address' not in kwargs:
+            if 'address' not in icomm_kwargs:
+                iargs, ikwargs = icomm_class.new_comm_kwargs(icomm_name,
+                                                             **icomm_kwargs)
+            if 'address' not in ocomm_kwargs:
+                oargs, okwargs = ocomm_class.new_comm_kwargs(ocomm_name,
+                                                             **ocomm_kwargs)
+            kwargs['address'] = _rpc_address_split.join([
+                ikwargs['address'], okwargs['address']])
+        else:
+            ikwargs['address'], okwargs['address'] = kwargs['address'].split(
+                _rpc_address_split)
+        ikwargs['name'] = icomm_name
+        okwargs['name'] = ocomm_name
+        ikwargs['comm'] = icomm_comm
+        okwargs['comm'] = ocomm_comm
+        kwargs['icomm_kwargs'] = ikwargs
+        kwargs['ocomm_kwargs'] = okwargs
         return args, kwargs
 
     def opp_comm_kwargs(self):
