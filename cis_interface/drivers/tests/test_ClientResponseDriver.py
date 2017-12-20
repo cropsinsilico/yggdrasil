14504d6e1c7a3af8eac9bd1fd24021c9a176464a
Meagan Lang <langmm.astro@gmail.com>
2017-12-19 19:33:20 -0600

    Reduce sleep times for ClientResponseDriver tests

--- cis_interface/drivers/tests/test_ClientResponseDriver.py
+++ cis_interface/drivers/tests/test_ClientResponseDriver.py
@@ -10,8 +10,6 @@ class TestClientResponseParam(parent.TestConnectionParam):
         self.driver = 'ClientResponseDriver'
         self.args = None
         self.attr_list += ['comm', 'msg_id', 'response_address']
-        self.sleeptime = 0.5
-        self.timeout = 5.0
         self.comm_name = _default_comm
         self.server_comm = _default_comm
         self.icomm_name = self.server_comm
