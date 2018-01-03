659566efd2d6002ff23c6663ce88ee3ecd87d19d
langmm <cfh5058@gmail.com>
2018-01-03 22:38:23 +0000

    Conditional skip of ZMQ tests if the library not installed

--- cis_interface/drivers/tests/test_ZMQCommDriver.py
+++ cis_interface/drivers/tests/test_ZMQCommDriver.py
@@ -1,34 +1,25 @@
+import unittest
+import nose.tools as nt
+from cis_interface.tools import is_zmq_installed
 from cis_interface.drivers.tests import test_CommDriver as parent
 
 
+@unittest.skipIf(not is_zmq_installed(), "ZMQ library not installed")
 class TestZMQCommParam(parent.TestCommParam):
-    r"""Test parameters for the ZMQCommDriver class.
-
-    Attributes:
-        -
-
-    """
+    r"""Test parameters for the ZMQCommDriver class."""
     def __init__(self, *args, **kwargs):
         super(TestZMQCommParam, self).__init__(*args, **kwargs)
         self.driver = 'ZMQCommDriver'
         self.comm_name = 'ZMQComm'
     
 
+@unittest.skipIf(not is_zmq_installed(), "ZMQ library not installed")
 class TestZMQCommDriverNoStart(TestZMQCommParam, parent.TestCommDriverNoStart):
-    r"""Test class for the ZMQCommDriver class without start.
-
-    Attributes (in addition to parent class's):
-        -
-
-    """
+    r"""Test class for the ZMQCommDriver class without start."""
     pass
 
 
+@unittest.skipIf(not is_zmq_installed(), "ZMQ library not installed")
 class TestZMQCommDriver(TestZMQCommParam, parent.TestCommDriver):
-    r"""Test class for the ZMQCommDriver class.
-
-    Attributes (in addition to parent class's):
-        -
-
-    """
+    r"""Test class for the ZMQCommDriver class."""
     pass
