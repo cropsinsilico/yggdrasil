5a2132898f3bd5e0f604998489cfed21755f6c38
langmm <cfh5058@gmail.com>
2018-01-09 17:00:52 +0000

    Conditional include of regex vs regex.h

--- cis_interface/dataio/AsciiTable.h
+++ cis_interface/dataio/AsciiTable.h
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdint.h>
+#ifdef _WIN32
+#include <regex>
+#else
 #include <regex.h>
+#endif
 #include "AsciiFile.h"
 
 /*! @brief Flag for checking if AsciiTable.h has already been included.*/
