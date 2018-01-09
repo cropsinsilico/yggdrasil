2e8d6969d4982682b2070bb1d51a01d90a68bfa8
langmm <cfh5058@gmail.com>
2018-01-09 17:08:23 +0000

    Flake8 from debug

--- cis_interface/tests/test_runner.py
+++ cis_interface/tests/test_runner.py
@@ -2,7 +2,7 @@ import os
 import nose.tools as nt
 import signal
 from cis_interface import runner, tools
-from cis_interface.tests import yamls as sc_yamls
+# from cis_interface.tests import yamls as sc_yamls
 from cis_interface.examples import yamls as ex_yamls
 
 
