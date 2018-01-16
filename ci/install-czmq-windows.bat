e7eff320559654207c1797c8f2e35faa156eef6b
langmm <cfh5058@gmail.com>
2018-01-16 23:31:17 +0000

    Rework install and cache all source and build folders

--- ci/install-czmq-windows.bat
+++ ci/install-czmq-windows.bat
@@ -2,6 +2,7 @@
 
 :: validate environment
 if "%VSINSTALLDIR%" == "" @echo Error: Attempt to build without proper DevStudio environment.&@goto :done
+if "%ZMQINSTALLDIR%" == "" set ZMQINSTALLDIR=C:\projects
 
 :: record starting time
 set STARTTIME=%DATE% %TIME%
@@ -21,54 +22,51 @@ choco install make -y
 :: Install Libsodium
 :: if libsodium is on disk, the Windows build of libzmq will automatically use it
 ECHO Installing libsodium...
-git clone --depth 1 -b stable https://github.com/jedisct1/libsodium.git
-cd libsodium\builds\msvc\build
-CALL buildbase.bat ..\vs20%DIRVER%\libsodium.sln %VSVER%
-:: buildall.bat
-cd ..\..\..\..
+set LIBSODIUM_SOURCEDIR=%ZMQINSTALLDIR%\libsodium
+set LIBSODIUM_BUILDDIR=%LIBSODIUM_SOURCEDIR%\builds\msvc\build
+IF NOT EXIST %LIBSODIUM_SOURCEDIR% (
+    git clone --depth 1 -b stable https://github.com/jedisct1/libsodium.git %LIBSODIUM_SOURCEDIR%
+    cd %LIBSODIUM_BUILDDIR%
+    CALL buildbase.bat ..\vs20%DIRVER%\libsodium.sln %VSVER%
+)
 
 :: Install libzmq
 ECHO Installing libzmq...
-set LIBZMQ_SOURCEDIR=C:\projects\libzmq
-set LIBZMQ_BUILDDIR=C:\projects\build_libzmq
-git clone --depth 1 git://github.com/zeromq/libzmq.git %LIBZMQ_SOURCEDIR%
-md %LIBZMQ_BUILDDIR%
-cd %LIBZMQ_BUILDDIR%
-cmake -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd" -G "%CMAKE_GENERATOR%" %LIBZMQ_SOURCEDIR%
-msbuild /v:minimal /p:Configuration=Release libzmq.vcxproj
+set LIBZMQ_SOURCEDIR=%ZMQINSTALLDIR%\libzmq
+set LIBZMQ_BUILDDIR=%ZMQINSTALLDIR%\build_libzmq
 set ZEROMQ_INCLUDE_DIR="%LIBZMQ_SOURCEDIR%\include"
 set ZEROMQ_LIBRARY_DIR="%LIBZMQ_BUILDDIR%\lib\Release"
-move "%ZEROMQ_LIBRARY_DIR%\libzmq-*lib" "%ZEROMQ_LIBRARY_DIR%\zmq.lib"
+IF NOT EXIST %LIBZMQ_SOURCEDIR% (
+    git clone --depth 1 git://github.com/zeromq/libzmq.git %LIBZMQ_SOURCEDIR%
+)
+IF NOT EXIST %LIBZMQ_BUILDDIR% (
+    md %LIBZMQ_BUILDDIR%
+    cd %LIBZMQ_BUILDDIR%
+    cmake -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd" -G "%CMAKE_GENERATOR%" %LIBZMQ_SOURCEDIR%
+    msbuild /v:minimal /p:Configuration=Release libzmq.vcxproj
+    move "%ZEROMQ_LIBRARY_DIR%\libzmq-*lib" "%ZEROMQ_LIBRARY_DIR%\zmq.lib"
+)
 echo ZMQ_LIBRARY=%ZEROMQ_LIBRARY_DIR%
 ls %ZEROMQ_LIBRARY_DIR%
-:: cd %LIBZMQ_SOURCEDIR%\builds\msvc\build
-:: CALL build.bat
-:: CALL configure.bat
-:: cd build
-:: CALL buildall.bat
-:: cd ..\..\..\..
 
 :: Install czmq
 ECHO Installing czmq...
-set CZMQ_SOURCEDIR=C:\projects\czmq
-set CZMQ_BUILDDIR=C:\projects\build_czmq
-git clone git://github.com/zeromq/czmq.git %CZMQ_SOURCEDIR%
-md %CZMQ_BUILDDIR%
-cd %CZMQ_BUILDDIR%
-cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INCLUDE_PATH="%ZEROMQ_INCLUDE_DIR%" -D CMAKE_LIBRARY_PATH="%ZEROMQ_LIBRARY_DIR%" -D CMAKE_C_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_C_FLAGS_DEBUG="/MTd" %CZMQ_SOURCEDIR%
-msbuild /v:minimal /p:Configuration=Release czmq.vcxproj
+set CZMQ_SOURCEDIR=%ZMQINSTALLDIR%\czmq
+set CZMQ_BUILDDIR=%ZMQINSTALLDIR%\build_czmq
 set CZMQ_INCLUDE_DIR="%CZMQ_SOURCEDIR%\include"
 set CZMQ_LIBRARY_DIR="%CZMQ_BUILDDIR%\Release"
-:: move 
+IF NOT EXIST %CZMQ_SOURCEDIR% (
+    git clone git://github.com/zeromq/czmq.git %CZMQ_SOURCEDIR%
+)
+IF NOT EXIST %CZMQ_BUILDDIR% (
+    md %CZMQ_BUILDDIR%
+    cd %CZMQ_BUILDDIR%
+    cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INCLUDE_PATH="%ZEROMQ_INCLUDE_DIR%" -D CMAKE_LIBRARY_PATH="%ZEROMQ_LIBRARY_DIR%" -D CMAKE_C_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_C_FLAGS_DEBUG="/MTd" %CZMQ_SOURCEDIR%
+    msbuild /v:minimal /p:Configuration=Release czmq.vcxproj
+    :: move
+)
 echo CZMQ_LIBRARY=%CZMQ_LIBRARY_DIR%
 ls %CZMQ_LIBRARY_DIR%
-:: cd %CZMQ_SOURCEDIR%\builds\msvc
-:: CALL configure.bat
-:: cd vs20%DIRVER%
-:: CALL build.bat
-:: cd build
-:: CALL buildall.bat
-:: cd ..\..\..\..
 
 :: Finalize and print stop time
 set STOPTIME=%DATE% %TIME%
@@ -76,4 +74,4 @@ set STOPTIME=%DATE% %TIME%
 @echo Start Time: %STARTTIME%
 
 :done
-@endlocal & set _return1=%ZEROMQ_LIBRARY_DIR%& set _return2=%CZMQ_LIBRARY_DIR%
+@endlocal & set LIBSODIUM_SOURCEDIR=%LIBSODIUM_SOURCEDIR% & set LIBZMQ_SOURCEDIR=%LIBZMQ_SOURCEDIR% & set LIBZMQ_BUILDDIR=%LIBZMQ_BUILDDIR% & set ZEROMQ_LIBRARY_DIR=%ZEROMQ_LIBRARY_DIR% & set CZMQ_SOURCEDIR=%CZMQ_SOURCEDIR% & set CZMQ_BUILDDIR=%CZMQ_BUILDDIR% & set CZMQ_LIBRARY_DIR=%CZMQ_LIBRARY_DIR%
