b896859055b39af956dd09811eb607ea5e8d8e8a
langmm <cfh5058@gmail.com>
2018-01-17 05:52:02 +0000

    Use environment variable

--- ci/install-czmq-windows.bat
+++ ci/install-czmq-windows.bat
@@ -46,7 +46,7 @@ IF NOT EXIST %LIBZMQ_BUILDDIR% (
     cmake -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd" -G "%CMAKE_GENERATOR%" %LIBZMQ_SOURCEDIR%
     ECHO Building zmq...
     ls %LIBZMQ_BUILDDIR%
-    msbuild /v:minimal /p:Configuration=StaticRelease /p:Platform=x64 libzmq.vcxproj
+    msbuild /v:minimal /p:Configuration=StaticRelease /p:Platform=%PLATFORM% libzmq.vcxproj
     ECHO Copying zmq lib...
     move "%ZEROMQ_LIBRARY_DIR%\libzmq-*lib" "%ZEROMQ_LIBRARY_DIR%\zmq.lib"
 )
@@ -68,7 +68,7 @@ IF NOT EXIST %CZMQ_BUILDDIR% (
     cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INCLUDE_PATH="%ZEROMQ_INCLUDE_DIR%" -D CMAKE_LIBRARY_PATH="%ZEROMQ_LIBRARY_DIR%" -D CMAKE_C_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_C_FLAGS_DEBUG="/MTd" %CZMQ_SOURCEDIR%
     ls %CZMQ_BUILDDIR%
     ECHO Building czmq...
-    msbuild /v:minimal /p:Configuration=StaticRelease /p:Platform=x64 czmq.vcxproj
+    msbuild /v:minimal /p:Configuration=StaticRelease /p:Platform=%PLATFORM% czmq.vcxproj
 )
 
 :: Finalize and print stop time
