9f898c7f4216f9a56eb634e8f80c04531d951052
langmm <cfh5058@gmail.com>
2018-01-18 23:31:55 +0000

    Remove all but input decl

--- cis_interface/examples/gs_lesson3/src/gs_lesson3.c
+++ cis_interface/examples/gs_lesson3/src/gs_lesson3.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
   printf("in gs_lesson4\n");
   psiInput_t in_channel = psiInput("input");
-  psiOutput_t out_channel = psiOutput("output");
+  // psiOutput_t out_channel = psiOutput("output");
 
   /* // Declare resulting variables and create buffer for received message */
   /* int flag = 1; */
@@ -41,8 +41,8 @@ int main(int argc, char *argv[]) {
   /* } */
 
   // Free input/output channels
-  psi_free(&in_channel);
-  psi_free(&out_channel);
+  /* psi_free(&in_channel); */
+  /* psi_free(&out_channel); */
   
   return 0;
 }
