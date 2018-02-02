95441390874e358b73b32a0190181e3ea78f8f16
Meagan Lang <langmm.astro@gmail.com>
2018-02-02 09:57:11 -0600

    Move FOR_DISABLE_CONSOLE_CTRL_HANDLER set to platform

--- cis_interface/__init__.py
+++ cis_interface/__init__.py
@@ -1,6 +1,5 @@
 r"""This package provides a framework for integrating models across languages
 such that they can be run simultaneously, passing input back and forth."""
-import os
 from cis_interface import backwards
 from cis_interface import platform
 from cis_interface import config
@@ -11,12 +10,6 @@ from cis_interface import dataio
 from cis_interface import tests
 from cis_interface import examples
 from cis_interface import runner
-
-
-# This is required to fix crash on Windows in case of Ctrl+C
-# https://github.com/ContinuumIO/anaconda-issues/issues/905#issuecomment-232498034
-if platform._is_win:
-    os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
 
 __all__ = ['backwards', 'platform', 'config', 'tools',
