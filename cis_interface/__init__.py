383e739ad923eff2d5af39f6a94b4c220479af15
Meagan Lang <langmm.astro@gmail.com>
2018-02-02 09:02:25 -0600

    Try runner tests first and disable new pgrp

--- cis_interface/__init__.py
+++ cis_interface/__init__.py
@@ -1,6 +1,6 @@
 r"""This package provides a framework for integrating models across languages
 such that they can be run simultaneously, passing input back and forth."""
-# import os
+import os
 from cis_interface import backwards
 from cis_interface import platform
 from cis_interface import config
@@ -15,8 +15,8 @@ from cis_interface import runner
 
 # This is required to fix crash on Windows in case of Ctrl+C
 # https://github.com/ContinuumIO/anaconda-issues/issues/905#issuecomment-232498034
-# if platform._is_win:
-#     os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
+if platform._is_win:
+    os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
 
 __all__ = ['backwards', 'platform', 'config', 'tools',
