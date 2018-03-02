55b40edddebe8d35d3ad9614923e5c7f777c622a
Meagan Lang <langmm.astro@gmail.com>
2018-03-02 15:49:51 -0600

    Remove quotes

--- ci/install-rmqserver-windows.ps1
+++ ci/install-rmqserver-windows.ps1
@@ -1,3 +1,3 @@
 $webclient=New-Object System.Net.WebClient
-$webclient.DownloadFile("$env:ERLANGURL", "$env:ERLANGEXE")
-$webclient.DownloadFile("$env:RMQURL", "$env:RMQEXE")
+$webclient.DownloadFile($env:ERLANGURL, $env:ERLANGEXE)
+$webclient.DownloadFile($env:RMQURL, $env:RMQEXE)
