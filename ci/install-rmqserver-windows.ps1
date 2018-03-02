e0fb5be51807597bffa6f6e3ff7e2c9766faec3a
Meagan Lang <langmm.astro@gmail.com>
2018-03-02 16:13:21 -0600

    Use single quotes

--- ci/install-rmqserver-windows.ps1
+++ ci/install-rmqserver-windows.ps1
@@ -1,3 +1,5 @@
+echo $env:ERLANGURL
+echo $env:RMQURL
 $webclient=New-Object System.Net.WebClient
-$webclient.DownloadFile($env:ERLANGURL, $env:ERLANGEXE)
-$webclient.DownloadFile($env:RMQURL, $env:RMQEXE)
+$webclient.DownloadFile('$env:ERLANGURL', '$env:ERLANGEXE')
+$webclient.DownloadFile('$env:RMQURL', '$env:RMQEXE')
