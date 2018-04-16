b3cc061ad2d7d09dec2f88e8ba337126e34ace43
langmm <cfh5058@gmail.com>
2018-04-16 20:07:02 +0000

    Accept any ZMQError for retry on bind to prevent issues with changes in errno across platforms

--- cis_interface/communication/ZMQComm.py
+++ cis_interface/communication/ZMQComm.py
@@ -134,8 +134,9 @@ def bind_socket(socket, address, retry_timeout=-1):
             port = socket.bind_to_random_port(address)
             address += ":%d" % port
     except zmq.ZMQError as e:  # pragma: debug
-        if (e.errno not in [48, 98]) or (retry_timeout < 0):
-            print(e, e.errno)
+        if (retry_timeout < 0):
+            # if (e.errno not in [48, 98]) or (retry_timeout < 0):
+            # print(e, e.errno)
             raise e
         else:
             logging.debug("Retrying bind in %f s", retry_timeout)
