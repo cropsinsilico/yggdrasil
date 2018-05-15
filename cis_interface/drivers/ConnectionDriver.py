cfa5643c7f5e89815e86de05620de6dc312ffc77
langmm <cfh5058@gmail.com>
2018-05-15 18:33:24 +0000

    Do constant sleep in connection driver

--- cis_interface/drivers/ConnectionDriver.py
+++ cis_interface/drivers/ConnectionDriver.py
@@ -314,11 +314,7 @@ class ConnectionDriver(Driver):
         self.state = 'before loop'
         try:
             self.open_comm()
-            T = self.start_timeout(10 * self.timeout)
-            while (not T.is_out) and (not self.is_valid):
-                self.sleep()
-            self.stop_timeout()
-            # self.sleep()  # Help ensure senders/receivers connected before messages
+            self.sleep()  # Help ensure senders/receivers connected before messages
             self.debug('Running in %s, is_valid = %s', os.getcwd(), str(self.is_valid))
             assert(self.is_valid)
         except BaseException:  # pragma: debug
