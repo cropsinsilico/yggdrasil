19030dc8e5c7725a564f42760cc781da9f44e863
Meagan Lang <langmm.astro@gmail.com>
2018-10-16 14:45:57 -0500

    Added example shoot/root integration for paper

---
+++ cis_interface/examples/paper/paper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ----------------Your Commands------------------- #
+case $1 in
+    --root )
+	echo "Running Root Model"
+	cisrun root.yml root_files.yml
+	;;
+    --shoot )
+	echo "Running Shoot Model"
+	cisrun shoot.yml shoot_files.yml
+	;;
+    * )
+	echo "Running Integration"
+	cisrun root.yml shoot.yml paper.yml
+	;;
+esac
