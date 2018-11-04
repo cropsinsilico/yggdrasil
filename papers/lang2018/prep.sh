5604b8eeb2626806f1c79ce51393b66658b9d642
Meagan Lang <langmm.astro@gmail.com>
2018-11-04 12:00:45 -0600

    Add missing paper files and ignore latex temp files

---
+++ papers/lang2018/prep.sh
@@ -0,0 +1,6 @@
+cp /Users/langmm/Library/texmf/bibtex/bib/CiS.bib /Users/langmm/cis_interface/papers/lang2018/
+cp /Users/langmm/cis_interface/images/compare_timed_pipe_commtype_python_python_Linux_27_average.png /Users/langmm/cis_interface/papers/lang2018/images/scaling_commtype.png
+cp /Users/langmm/cis_interface/images/compare_timed_pipe_language_ZMQComm_Linux_27_average.png /Users/langmm/cis_interface/papers/lang2018/images/scaling_language.png
+cp /Users/langmm/cis_interface/images/compare_timed_pipe_language_ZMQComm_MacOS_27_average.png /Users/langmm/cis_interface/papers/lang2018/images/scaling_language_matlab.png
+cp /Users/langmm/cis_interface/images/compare_timed_pipe_platform_ZMQComm_python_python_27_average.png /Users/langmm/cis_interface/papers/lang2018/images/scaling_platform.png
+cp /Users/langmm/cis_interface/images/compare_timed_pipe_python_ZMQComm_python_python_Linux_average.png /Users/langmm/cis_interface/papers/lang2018/images/scaling_python.png
