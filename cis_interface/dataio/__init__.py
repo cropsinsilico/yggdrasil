133b1ca414983b7cc8eaf6f90c48712a51b60a74
Meagan Lang <langmm.astro@gmail.com>
2019-01-16 11:35:28 -0600

    Remove python dataio classes as they are deprecated and not longer used for serialization

--- cis_interface/dataio/__init__.py
+++
@@ -1,7 +0,0 @@
-r"""Routines for standardized I/O."""
-
-from cis_interface.dataio import AsciiFile
-from cis_interface.dataio import AsciiTable
-from cis_interface.dataio import tests
-
-__all__ = ['AsciiFile', 'AsciiTable', 'tests']
