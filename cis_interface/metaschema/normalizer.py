5585e13a2825a6efe9dfa7a29efa4f127802d507
Meagan Lang <langmm.astro@gmail.com>
2019-01-23 12:59:57 -0600

    Re-formulate normalizer to be coverage friendly

--- cis_interface/metaschema/normalizer.py
+++ cis_interface/metaschema/normalizer.py
@@ -183,12 +183,10 @@ def create(*args, **kwargs):
             for k, v in kwargs.items():
                 if k == 'normalizers':
                     for ik, iv in self.NORMALIZERS.items():
-                        if ik not in v:
-                            v[ik] = iv
+                        v.setdefault(ik, iv)
                 elif k == 'validators':
                     for ik, iv in self.VALIDATORS.items():
-                        if ik not in v:
-                            v[ik] = iv
+                        v.setdefault(ik, iv)
                 if hasattr(self, k.upper()):
                     ksub = k.upper()
                 else:
