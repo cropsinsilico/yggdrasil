614984da5ca03968325ec1119b9741057a397ae4
Meagan Lang <langmm.astro@gmail.com>
2019-01-23 16:03:19 -0600

    Improve coverage of SchemaMetaschemaType

--- cis_interface/metaschema/datatypes/tests/test_SchemaMetaschemaType.py
+++ cis_interface/metaschema/datatypes/tests/test_SchemaMetaschemaType.py
@@ -17,4 +17,9 @@ class TestSchemaMetaschemaType(parent.TestJSONObjectMetaschemaType):
         self._valid_decoded = [self._value]
         self._invalid_encoded = [{}]
         self._compatible_objects = [(self._value, self._value, None)]
-        self._valid_normalize += [('float', {'type': 'float'})]
+        self._valid_normalize += [('float', {'type': 'float'}),
+                                  ({'units': 'g'}, {'units': 'g',
+                                                    'type': 'scalar',
+                                                    'subtype': 'float',
+                                                    'precision': 64}),
+                                  ({'title': 'a'}, {'title': 'a'})]
