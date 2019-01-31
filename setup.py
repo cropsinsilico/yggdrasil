fa15809f6be724cefc473e564635267d524e6c0d
Meagan Lang <langmm.astro@gmail.com>
2019-01-30 20:11:47 -0600

    Update appveyor to clone submodules and add check in setup.py for command line option to specify location of rapidjson source files and check that they exist.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import versioneer
 import install_matlab_engine
 import create_coveragerc
 cis_ver = versioneer.get_version()
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 # Attempt to install openalea
@@ -26,6 +27,37 @@ else:
     warnings.warn("Could not import matlab.engine. " +
                   "Matlab features will be disabled.")
     matlab_installed = False
+
+
+# Determine if rapidjson installed and parse user defined location
+rj_include_dir0 = os.path.join(ROOT_PATH, 'cis_interface', 'rapidjson', 'include')
+for idx, arg in enumerate(sys.argv[:]):
+    if ((arg.startswith('--rj-include-dir=')
+         or arg.startswith('--rapidjson-include-dir'))):
+        sys.argv.pop(idx)
+        rj_include_dir = os.path.abspath(arg.split('=', 1)[1])
+        break
+else:
+    rj_include_dir = rj_include_dir0
+if not os.path.isdir(rj_include_dir):
+    raise RuntimeError("RapidJSON sources could not be located. If you "
+                       "cloned the git repository, initialize the rapidjson "
+                       "git submodule by calling "
+                       "'git submodule update --init --recursive' "
+                       "from inside the repository.")
+if rj_include_dir != rj_include_dir0:
+    def_config_file = os.path.join(ROOT_PATH, 'cis_interface', 'defaults.cfg')
+    try:
+        import ConfigParser as configparser
+    except ImportError:
+        import configparser
+    cfg = configparser.ConfigParser()
+    cfg.read(def_config_file)
+    if not cfg.has_section('c'):
+        cfg.add_section('c')
+    cfg.set('c', 'rapidjson_include', rj_include_dir)
+    with open(def_config_file, 'w') as fd:
+        cfg.write(fd)
 
     
 # Set coverage options in .coveragerc
