e032f51af513d2127f8b220484604d6b51968514
Meagan Lang <langmm.astro@gmail.com>
2019-02-08 16:46:13 -0600

    Change serialization tests to use class level attributes and reduce warnings

--- yggdrasil/serialize/tests/test_PandasSerialize.py
+++ yggdrasil/serialize/tests/test_PandasSerialize.py
@@ -6,9 +6,7 @@ from yggdrasil.serialize.tests import test_AsciiTableSerialize as parent
 class TestPandasSerialize(parent.TestAsciiTableSerialize):
     r"""Test class for TestPandasSerialize class."""
 
-    def __init__(self, *args, **kwargs):
-        super(TestPandasSerialize, self).__init__(*args, **kwargs)
-        self._cls = 'PandasSerialize'
+    _cls = 'PandasSerialize'
 
     def test_apply_field_names_errors(self):
         r"""Test errors raised by apply_field_names."""
