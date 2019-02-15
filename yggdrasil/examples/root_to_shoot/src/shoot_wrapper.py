54ede7a9fc5dd5e31753774140baa3a3dce33a1a
Meagan Lang <langmm.astro@gmail.com>
2019-02-15 15:48:07 -0600

    Fix errors when using pint

--- yggdrasil/examples/root_to_shoot/src/shoot_wrapper.py
+++ yggdrasil/examples/root_to_shoot/src/shoot_wrapper.py
@@ -15,19 +15,19 @@ NextShootMass = YggOutput('next_shoot_mass', '%lf\n')
 flag, r_s = ShootGrowthRate.recv()
 if not flag:
     raise RuntimeError('shoot: Error receiving shoot growth rate.')
-print('shoot: Received shoot growth rate: {:f}'.format(r_s))
+print('shoot: Received shoot growth rate: %s' % str(r_s))
 
 # Receive initial shoot mass
 flag, S_t = InitShootMass.recv()
 if not flag:
     raise RuntimeError('shoot: Error receiving initial shootmass.')
-print('shoot: Received initial shoot mass: {:f}'.format(S_t))
+print('shoot: Received initial shoot mass: %s' % str(S_t))
 
 # Receive inital root mass
 flag, R_t = NextRootMass.recv()
 if not flag:
     raise RuntimeError('shoot: Error receiving initial root mass.')
-print('shoot: Received initial root mass: {:f}'.format(R_t))
+print('shoot: Received initial root mass: %s' % str(R_t))
 
 # Send initial shoot mass
 flag = NextShootMass.send(S_t)
@@ -43,18 +43,18 @@ while True:
     if not flag:
         print('shoot: No more time steps.')
         break
-    print('shoot: Received next time step: {:f}'.format(dt))
+    print('shoot: Received next time step: %s' % str(dt))
 
     # Receive the next root mass
     flag, R_tp1 = NextRootMass.recv()
     if not flag:
         # This raises an error because there must be a root mass for each time step
         raise RuntimeError('shoot: Error receiving root mass for timestep %d.' % (i + 1))
-    print('shoot: Received next root mass: {:f}'.format(R_tp1))
+    print('shoot: Received next root mass: %s' % str(R_tp1))
 
     # Calculate shoot mass
     S_tp1 = calc_shoot_mass(r_s, dt, S_t, R_t, R_tp1)
-    print('shoot: Calculated next shoot mass: {:f}'.format(S_tp1))
+    print('shoot: Calculated next shoot mass: %s' % str(S_tp1))
 
     # Output shoot mass
     flag = NextShootMass.send(S_tp1)
