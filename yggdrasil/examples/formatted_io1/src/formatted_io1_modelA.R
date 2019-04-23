97a5af6b3fd0bda4ecb1dbb5f4132f0425771e40
Meagan Lang <langmm.astro@gmail.com>
2019-04-23 09:06:10 -0500

    Added formatted_io1 example in R

---
+++ yggdrasil/examples/formatted_io1/src/formatted_io1_modelA.R
@@ -0,0 +1,31 @@
+# Import library for input/output
+library(reticulate)
+library(zeallot)
+ygg <- import('yggdrasil.interface.YggInterface')
+
+# Initialize input/output channels
+in_channel <- ygg$YggInput('inputA')
+out_channel <- ygg$YggOutput('outputA')
+
+# Loop until there is no longer input or the queues are closed
+while(TRUE) {
+
+  # Receive input from input channel
+  # If there is an error, the flag will be False
+  c(flag, msg) %<-% in_channel$recv()
+  if (!flag) {
+    print('Model A: No more input.')
+    break
+  }
+
+  # Print received message
+  print(sprintf('Model A: %s', msg))
+
+  # Send output to output channel
+  # If there is an error, the flag will be False
+  flag = out_channel$send(msg)
+  if (!flag) {
+    stop('Model A: Error sending output.')
+  }
+
+}
