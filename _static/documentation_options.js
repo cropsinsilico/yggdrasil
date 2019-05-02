8467279f30ddc3f05daa62e992796276251e642a
Meagan Lang <langmm.astro@gmail.com>
2019-05-01 19:17:14 -0500

    rebuilt docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.7+10.gd8f2fc6d.dirty',
+    VERSION: '0.8.7+11.g7573c765.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
