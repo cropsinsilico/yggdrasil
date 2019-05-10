1028efd4f77f4a3e16afb2c389fd95c0349bcf69
Meagan Lang <langmm.astro@gmail.com>
2019-05-10 14:39:19 -0500

    Return version of openalea.lpy

--- yggdrasil/drivers/LPyModelDriver.py
+++ yggdrasil/drivers/LPyModelDriver.py
@@ -16,3 +16,20 @@ class LPyModelDriver(PythonModelDriver):  # pragma: lpy
     default_interpreter_flags = [_model_script]
     interface_dependencies = ['openalea.lpy']
     function_param = None
+
+    @classmethod
+    def language_version(cls, **kwargs):
+        r"""Determine the version of this language.
+
+        Args:
+            **kwargs: Keyword arguments are passed to cls.run_executable.
+
+        Returns:
+            str: Version of compiler/interpreter for this language.
+
+        """
+        try:
+            import openalea.lpy
+            return openalea.lpy.__version__.LPY_VERSION_STR
+        except ImportError:
+            raise RuntimeError("openalea.lpy not installed.")
