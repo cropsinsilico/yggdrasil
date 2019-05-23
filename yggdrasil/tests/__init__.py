e1bb82fd5ff042d0c3f9f3516ad72a6804e713a9
Meagan Lang <langmm.astro@gmail.com>
2019-05-23 12:59:36 -0500

    Improve display and recursion for testing equality testing

--- yggdrasil/tests/__init__.py
+++ yggdrasil/tests/__init__.py
@@ -75,10 +75,181 @@ shutil.copy(makefile0, os.path.join(script_dir, "Makefile"))
 enable_long_tests = tools.check_environ_bool("YGG_ENABLE_LONG_TESTS")
 
 
+# Wrapped class to allow handling of arrays
+class WrappedTestCase(unittest.TestCase):  # pragma: no cover
+    def __init__(self, *args, **kwargs):
+        super(WrappedTestCase, self).__init__(*args, **kwargs)
+        self.addTypeEqualityFunc(units._unit_quantity, 'assertUnitsEqual')
+        self.addTypeEqualityFunc(units._unit_array, 'assertUnitsEqual')
+        self.addTypeEqualityFunc(np.ndarray, 'assertArrayEqual')
+        self.addTypeEqualityFunc(pd.DataFrame, 'assertArrayEqual')
+
+    def has_units(self, obj):
+        if isinstance(obj, (list, tuple)):
+            for x in obj:
+                if self.has_units(x):
+                    return True
+        elif isinstance(obj, dict):
+            for x in obj.values():
+                if self.has_units(x):
+                    return True
+        else:
+            return units.has_units(obj)
+        return False
+
+    def _getAssertEqualityFunc(self, first, second):
+        # Allow comparison of tuple to list and units to anything
+        if (type(first), type(second)) in [(list, tuple), (tuple, list)]:
+            return self.assertSequenceEqual
+        elif units.has_units(first) or units.has_units(second):
+            return self.assertUnitsEqual
+        return super(WrappedTestCase, self)._getAssertEqualityFunc(first, second)
+        
+    def assertEqual(self, first, second, msg=None, dont_nest=False):
+        r"""Fail if the two objects are unequal as determined by the '=='
+           operator."""
+        if (not dont_nest):
+            # Do nested evaluation for objects containing units
+            if (self.has_units(first) or self.has_units(second)):
+                self.assertEqualNested(first, second, msg=msg)
+                return
+        try:
+            super(WrappedTestCase, self).assertEqual(first, second, msg=msg)
+        except ValueError:
+            if dont_nest:
+                raise
+            self.assertEqualNested(first, second, msg=msg)
+
+    def assertEqualNested(self, first, second, msg=None):
+        r"""Fail if the two objects are unequal as determined by descending
+        recursively into the object if it is a list, tuple, or dictionary."""
+        if isinstance(first, list):
+            self.assertSequenceEqualNested(first, second, msg=msg, seq_type=list)
+        elif isinstance(first, tuple):
+            self.assertSequenceEqualNested(first, second, msg=msg, seq_type=tuple)
+        elif isinstance(first, dict):
+            self.assertDictEqualNested(first, second, msg=msg)
+        else:
+            self.assertEqual(first, second, msg=msg, dont_nest=True)
+    
+    def assertSequenceEqualNested(self, seq1, seq2, msg=None, seq_type=None):
+        if seq_type is not None:
+            seq_type_name = seq_type.__name__
+            # Currently it makes more sense to allow equality between lists and
+            # tuples
+            # if not isinstance(seq1, seq_type):
+            #     raise self.failureException(
+            #         'First sequence is not a %s: %s'
+            #         % (seq_type_name, unittest.util.safe_repr(seq1)))
+            # if not isinstance(seq2, seq_type):
+            #     raise self.failureException(
+            #         'Second sequence is not a %s: %s'
+            #         % (seq_type_name, unittest.util.safe_repr(seq2)))
+        else:
+            seq_type_name = "sequence"
+
+        differing = None
+        try:
+            len1 = len(seq1)
+        except (TypeError, NotImplementedError):
+            differing = 'First %s has no length.    Non-sequence?' % (
+                seq_type_name)
+
+        if differing is None:
+            try:
+                len2 = len(seq2)
+            except (TypeError, NotImplementedError):
+                differing = 'Second %s has no length.    Non-sequence?' % (
+                    seq_type_name)
+
+        if differing is None:
+            seq1_repr = unittest.util.safe_repr(seq1)
+            seq2_repr = unittest.util.safe_repr(seq2)
+            if len(seq1_repr) > 30:
+                seq1_repr = seq1_repr[:30] + '...'
+            if len(seq2_repr) > 30:
+                seq2_repr = seq2_repr[:30] + '...'
+            elements = (seq_type_name.capitalize(), seq1_repr, seq2_repr)
+            differing = '%ss differ: %s != %s\n' % elements
+
+            for i in range(min(len1, len2)):
+                try:
+                    item1 = seq1[i]
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('\nUnable to index element %d of first %s\n'
+                                  % (i, seq_type_name))
+                    break
+
+                try:
+                    item2 = seq2[i]
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('\nUnable to index element %d of second %s\n'
+                                  % (i, seq_type_name))
+                    break
+
+                self.assertEqualNested(
+                    item1, item2,
+                    msg=differing + '\nFirst differing element at index %d' % i)
+            else:
+                if (len1 == len2):
+                    return
+
+            if len1 > len2:
+                differing += ('\nFirst %s contains %d additional '
+                              'elements.\n' % (seq_type_name, len1 - len2))
+                try:
+                    differing += ('First extra element %d:\n%s\n' %
+                                  (len2, unittest.util.safe_repr(seq1[len2])))
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('Unable to index element %d '
+                                  'of first %s\n' % (len2, seq_type_name))
+            elif len1 < len2:
+                differing += ('\nSecond %s contains %d additional '
+                              'elements.\n' % (seq_type_name, len2 - len1))
+                try:
+                    differing += ('First extra element %d:\n%s\n' %
+                                  (len1, unittest.util.safe_repr(seq2[len1])))
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('Unable to index element %d '
+                                  'of second %s\n' % (len1, seq_type_name))
+        standardMsg = differing
+        diffMsg = '\n' + '\n'.join(
+            difflib.ndiff(pprint.pformat(seq1).splitlines(),
+                          pprint.pformat(seq2).splitlines()))
+        standardMsg = self._truncateMessage(standardMsg, diffMsg)
+        msg = self._formatMessage(msg, standardMsg)
+        self.fail(msg)
+        
+    def assertDictEqualNested(self, d1, d2, msg=None):
+        self.assertIsInstance(d1, dict, 'First argument is not a dictionary')
+        self.assertIsInstance(d2, dict, 'Second argument is not a dictionary')
+        self.assertEqual(sorted(list(d1.keys())), sorted(list(d2.keys())),
+                         'Dictionaries do not have the same keys')
+        for k in d1.keys():
+            standardMsg = 'Value for key %s differs' % k
+            msg_k = self._formatMessage(msg, standardMsg)
+            self.assertEqual(d1[k], d2[k], msg=msg_k)
+
+    def assertUnitsEqual(self, first, second, msg=None):
+        r"""Assertion for equality in case of objects with units."""
+        if units.has_units(first) and units.has_units(second):
+            first = units.convert_to(first, units.get_units(second))
+        self.assertEqual(units.get_data(first), units.get_data(second))
+        
+    def assertArrayEqual(self, first, second, msg=None):
+        r"""Assertion for equality in case of arrays."""
+        try:
+            np.testing.assert_array_equal(first, second)
+        except AssertionError as e:
+            standardMsg = str(e)
+            msg = self._formatMessage(msg, standardMsg)
+            raise self.failureException(msg)
+
+
 if backwards.PY2:  # pragma: Python 2
     # Dummy TestCase instance, so we can initialize an instance
     # and access the assert instance methods
-    class DummyTestCase(unittest.TestCase):  # pragma: no cover
+    class DummyTestCase(WrappedTestCase):  # pragma: no cover
         def __init__(self):
             super(DummyTestCase, self).__init__('_dummy')
 
@@ -100,7 +271,7 @@ if backwards.PY2:  # pragma: Python 2
         
 else:  # pragma: Python 3
 
-    ut = unittest.TestCase()
+    ut = WrappedTestCase()
 
 
 def long_running(func):
@@ -180,44 +351,7 @@ def assert_equal(x, y):
         AssertionError: If the two messages are not equivalent.
 
     """
-    if isinstance(y, (list, tuple)):
-        assert(isinstance(x, (list, tuple)))
-        try:
-            ut.assertEqual(len(x), len(y))
-        except AssertionError:  # pragma: debug
-            print('first = %s\nsecond = %s\n' % (
-                pprint.pformat(x), pprint.pformat(y)))
-            raise
-        for ix, iy in zip(x, y):
-            assert_equal(ix, iy)
-    elif isinstance(y, dict):
-        assert(issubclass(y.__class__, dict))
-        # ut.assertEqual(type(x), type(y))
-        try:
-            ut.assertEqual(len(x), len(y))
-        except AssertionError:  # pragma: debug
-            print('first = %s\nsecond = %s\n' % (
-                pprint.pformat(x), pprint.pformat(y)))
-            raise
-        for k, iy in y.items():
-            ix = x[k]
-            assert_equal(ix, iy)
-    elif isinstance(y, (np.ndarray, pd.DataFrame)):
-        if units.has_units(y) and (not units.has_units(x)):  # pragma: debug
-            y = units.get_data(y)
-        elif (not units.has_units(y)) and units.has_units(x):
-            x = units.get_data(x)
-        np.testing.assert_array_equal(x, y)
-    else:
-        if units.has_units(y) and units.has_units(x):
-            x = units.convert_to(x, units.get_units(y))
-            assert_equal(units.get_data(x), units.get_data(y))
-        else:
-            if units.has_units(y) and (not units.has_units(x)):  # pragma: debug
-                y = units.get_data(y)
-            elif (not units.has_units(y)) and units.has_units(x):
-                x = units.get_data(x)
-            ut.assertEqual(x, y)
+    ut.assertEqual(x, y)
 
 
 def assert_not_equal(x, y):
