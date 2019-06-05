2ecf4bfdd29d387b61d37414584dacc2944ae8bb
Meagan Lang <langmm.astro@gmail.com>
2019-06-05 09:19:58 -0500

    Explicitly set default_compiler based on platform to prevent accidentally using make to compile regular C

--- yggdrasil/drivers/CModelDriver.py
+++ yggdrasil/drivers/CModelDriver.py
@@ -243,8 +243,13 @@ class CModelDriver(CompiledModelDriver):
     def after_registration(cls):
         r"""Operations that should be performed to modify class attributes after
         registration."""
-        if platform._is_mac and (cls.default_compiler is None):
-            cls.default_compiler = 'clang'
+        if cls.default_compiler is None:
+            if platform._is_linux:
+                cls.default_compiler = 'gcc'
+            elif platform._is_mac:
+                cls.default_compiler = 'clang'
+            elif platform._is_win:  # pragma: windows
+                cls.default_compiler = 'cl'
         CompiledModelDriver.after_registration(cls)
         archiver = cls.get_tool('archiver')
         linker = cls.get_tool('linker')
