4b6845ef6e8adfcca8689bca0aafd2fce569d613
Meagan Lang <langmm.astro@gmail.com>
2019-06-10 14:14:54 -0500

    Try using .exe extension on R executable

--- yggdrasil/languages/install_interface_R.bat
+++ yggdrasil/languages/install_interface_R.bat
@@ -1,3 +1,3 @@
-R CMD build R
-R -e 'install.packages(c("reticulate", "zeallot", "float", "bit64"), repos="https://cloud.r-project.org")'
-R CMD INSTALL yggdrasil_0.1.tar.gz
+R.exe CMD build R
+R.exe -e 'install.packages(c("reticulate", "zeallot", "float", "bit64"), repos="https://cloud.r-project.org")'
+R.exe CMD INSTALL yggdrasil_0.1.tar.gz
