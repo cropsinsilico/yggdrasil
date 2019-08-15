a40e0e676a70c73139160b2a59fe3a7352aa675f
Meagan Lang <langmm.astro@gmail.com>
2019-08-15 18:41:02 -0500

    Only use test that requires assertRaises as context manager on python >2

--- yggdrasil/languages/tests/test_install_languages.py
+++ yggdrasil/languages/tests/test_install_languages.py
@@ -1,18 +1,18 @@
 import unittest
-from yggdrasil import platform
+from yggdrasil import backwards
 from yggdrasil.languages import install_languages
 
 
-@unittest.skipIf(platform._is_win, "Error in unittest on windows")
 class TestInstallLanguages(unittest.TestCase):
 
     def test_update_argparser(self):
         r"""Test update_argparser."""
         install_languages.update_argparser(arglist=['all'])
         install_languages.update_argparser(arglist=['python'])
-        with self.assertRaises(SystemExit):
-            install_languages.update_argparser(arglist=['-h'])
-            unittest.main(exit=False)
+        if not backwards.PY2:  # pragma: Python 2
+            with self.assertRaises(SystemExit):
+                install_languages.update_argparser(arglist=['-h'])
+                unittest.main(exit=False)
 
     def test_install_language(self):
         r"""Test install_language."""
