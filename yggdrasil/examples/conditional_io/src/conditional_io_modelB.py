96dc31f1e71934091f811a979f743e1feea56fbd
Meagan Lang <langmm.astro@gmail.com>
2019-08-22 14:57:15 -0500

    Create Python example of conditional_io

---
+++ yggdrasil/examples/conditional_io/src/conditional_io_modelB.py
@@ -0,0 +1,10 @@
+def modelB_function1(in_val):
+    out_val = in_val**2
+    print("modelB_function1(%s) = %s" % (in_val, out_val))
+    return out_val
+
+
+def modelB_function2(in_val):
+    out_val = in_val**3
+    print("modelB_function2(%s) = %s" % (in_val, out_val))
+    return out_val
