3a5349a0604be43708ddc562e049c8e953106482
Meagan Lang <cfh5058@gmail.com>
2019-11-15 16:42:51 -0800

    Don't run types example for cmake and make langauges

--- yggdrasil/examples/__init__.py
+++ yggdrasil/examples/__init__.py
@@ -57,6 +57,8 @@ def register_example(example_dir):
         lang_avail += ['all', 'all_nomatlab', 'c', 'cpp', 'matlab', 'python']
     elif example_base == 'types':
         lang_avail += tools.get_supported_lang()
+        lang_avail.remove('cmake')
+        lang_avail.remove('make')
     else:
         lang_search = example_base + '_%s.yml'
     if lang_search is not None:
