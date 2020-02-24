e8eed9e799c9d39da8f3c1be4603a701d119febc
Meagan Lang <langmm.astro@gmail.com>
2020-02-24 11:43:51 -0600

    Add fortran version of formatted_io4 example

---
+++ yggdrasil/examples/formatted_io4/src/formatted_io4_modelB.f90
@@ -0,0 +1,50 @@
+program main
+  ! Include methods for input/output channels
+  use fygg
+
+  ! Declare resulting variables and create buffer for received message
+  integer :: flag = 1
+  type(yggcomm) :: in_channel, out_channel
+  integer(kind=c_size_t), target :: nrows
+  type(character_1d), target :: name
+  type(c_long_1d), target :: count
+  type(real8_1d), target :: siz
+  integer(kind=c_size_t) :: i
+
+  ! initialize input/output channels
+  in_channel = ygg_ascii_array_input("inputB")
+  out_channel = ygg_ascii_array_output("outputB", "%6s\t%ld\t%f\n")
+
+  ! Loop until there is no longer input or the queues are closed
+  do while (flag.ge.0)
+
+     ! Receive input from input channel
+     ! If there is an error, the flag will be negative
+     ! Otherwise, it is the number of variables filled
+     flag = ygg_recv_var_realloc(in_channel, [ &
+          yggarg(nrows), yggarg(name), yggarg(count), yggarg(siz)])
+     if (flag.lt.0) then
+        print *, "Model B: No more input."
+        exit
+     end if
+
+     ! Print received message
+     print *, "Model B: (", nrows, " rows)"
+     do i = 1, nrows
+        print *, "   ", name%x(i)%x, count%x(i), siz%x(i)
+     end do
+
+     ! Send output to output channel
+     ! If there is an error, the flag will be negative
+     flag = ygg_send_var(out_channel, [ &
+          yggarg(nrows), yggarg(name), yggarg(count), yggarg(siz)])
+     if (flag.lt.0) then
+        print *, "Model B: Error sending output."
+        exit
+     end if
+
+  end do
+
+  call exit(0)
+
+end program main
