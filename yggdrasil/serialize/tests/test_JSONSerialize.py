9a72ed2f07029ecf64a3988a091ec3248bd6b7e5
Meagan Lang <langmm.astro@gmail.com>
2020-03-12 20:44:59 -0500

    Create generic function for generating component tests. Change transforms and serializers to use the test generator.

--- yggdrasil/serialize/tests/test_JSONSerialize.py
+++
@@ -1,7 +0,0 @@
-from yggdrasil.serialize.tests import test_SerializeBase as parent
-
-
-class TestJSONSerialize(parent.TestSerializeBase):
-    r"""Test class for TestJSONSerialize class."""
-
-    _cls = 'JSONSerialize'
