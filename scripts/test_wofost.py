69fc9d3092cafaccb87137b50d242f0714b2aa87
Meagan Lang <langmm.astro@gmail.com>
2020-04-14 12:25:39 -0500

    Add scripts for testing wofost/netcdf. Remove .cab from gitignore as it is extension used by wofost.

---
+++ scripts/test_wofost.py
@@ -0,0 +1,34 @@
+import numpy as np
+from yggdrasil import units
+from yggdrasil.tests import assert_equal
+from yggdrasil.communication.NetCDFFileComm import NetCDFFileComm
+from yggdrasil.serialize.WOFOSTParamSerialize import WOFOSTParamSerialize
+
+fname_param = 'cropfile_example.cab'
+# fname_netcdf = 'simple.nc'
+fname_netcdf = 'test.nc'
+
+# Test serializer
+with open(fname_param, 'rb') as fd:
+    contents = fd.read()
+inst = WOFOSTParamSerialize()
+x = inst.deserialize(contents)
+
+
+# Test comm
+data = {
+    'time': units.add_units(np.arange(10).astype('float32'), 's'),
+    'x': np.array(['a', 'hello', 'c'], 'S5'),
+    'space': units.add_units(np.ones((5, 5), 'int64'), 'mol')}
+# out_file = NetCDFFileComm('test_send', address=fname_netcdf, direction='send')
+# assert(out_file.send(data))
+
+in_file = NetCDFFileComm('test_recv', address=fname_netcdf, direction='recv')
+flag, data_recv = in_file.recv()
+assert(flag)
+assert_equal(data_recv, data)
+
+import pprint; pprint.pprint(data)
+
+with open(fname_netcdf, 'rb') as fd:
+    print(fd.read())
