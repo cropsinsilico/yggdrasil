e5f0a7b3f8009166f320c826b7ac65fbd01faca1
Meagan Lang <langmm.astro@gmail.com>
2020-04-14 12:11:35 -0500

    Add debug messages to assignment for reallocatable character array in model_function example.

--- yggdrasil/languages/fortran/YggInterface_assign.f90
+++ yggdrasil/languages/fortran/YggInterface_assign.f90
@@ -2,10 +2,14 @@ subroutine yggassign_yggchar2character(in, out)
   type(yggchar_r), intent(in) :: in
   character(len=:), allocatable :: out
   integer :: i
+  print *, "yggassign_yggchar2character"
+  print *, "len = ", size(in%x)
   allocate(character(len=size(in%x)) :: out)
+  print *, "allocated"
   do i = 1, size(in%x)
      out(i:i) = in%x(i)
   end do
+  print *, "yggassign_yggchar2character returns"
 end subroutine yggassign_yggchar2character
 ! subroutine yggassign_character2yggchar(in, out)
 ! end subroutine yggassign_character2yggchar
