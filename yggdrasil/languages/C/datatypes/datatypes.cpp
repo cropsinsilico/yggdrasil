3a2d8976eda129bbff13f6f1022c23f5437fadb6
Meagan Lang <langmm.astro@gmail.com>
2020-04-17 16:07:58 -0500

    Handle case of negative return value from snprintf in header formatting on windows

--- yggdrasil/languages/C/datatypes/datatypes.cpp
+++ yggdrasil/languages/C/datatypes/datatypes.cpp
@@ -1883,12 +1883,16 @@ extern "C" {
       // Combine
       int ret = snprintf(*buf, buf_siz, "%s%s%s",
 			 MSG_HEAD_SEP, head_buf.GetString(), MSG_HEAD_SEP);
-      if ((size_t)ret > buf_siz) {
+      if ((ret < 0) || ((size_t)ret > buf_siz)) {
 	printf("format_comm_header (first): Header size (%d) exceeds buffer size (%lu).\n",
 	       ret, buf_siz);
 	ygglog_info("format_comm_header: Header size (%d) exceeds buffer size (%lu).",
 		    ret, buf_siz);
-	buf_siz = (size_t)(ret+1);
+	if (ret < 0) {
+	  buf_siz = 2 * buf_siz;
+	} else {
+	  buf_siz = (size_t)(ret+1);
+	}
 	buf[0] = (char*)realloc(buf[0], buf_siz);
 	ret = snprintf(*buf, buf_siz, "%s%s%s",
 		       MSG_HEAD_SEP, head_buf.GetString(), MSG_HEAD_SEP);
