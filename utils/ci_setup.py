8bd7f27ee6e446d1bebfed4afb619117e0a31552
Meagan Lang <langmm.astro@gmail.com>
2020-12-02 16:42:13 -0800

    Try original method of installing from a local conda-build (this had been broken in the past)

--- utils/ci_setup.py
+++ utils/ci_setup.py
@@ -284,8 +284,10 @@ def deploy_package_on_ci(method, verbose=False):
             "%s build %s --python %s %s" % (
                 conda_cmd, 'recipe', PYVER, build_flags),
             "%s index %s" % (conda_cmd, index_dir),
-            "%s install %s -c file:/%s/conda-bld yggdrasil" % (
-                conda_cmd, install_flags, prefix_dir),
+            "%s install %s --use-local yggdrasil" % (
+                conda_cmd, install_flags),
+            # "%s install %s -c file:/%s/conda-bld yggdrasil" % (
+            #     conda_cmd, install_flags, prefix_dir),
         ]
     elif method == 'pip':
         if verbose:
