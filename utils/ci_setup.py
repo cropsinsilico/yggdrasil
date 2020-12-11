62b4e26937205e5b352ea4ac121aef999eaece62
Meagan Lang <langmm.astro@gmail.com>
2020-12-11 13:55:56 -0800

    Try from file without --update-deps

--- utils/ci_setup.py
+++ utils/ci_setup.py
@@ -382,10 +382,10 @@ def deploy_package_on_ci(method, without_build=False, verbose=False):
             install_flags = '-q'
         cmds += [
             # "%s clean --all" % CONDA_CMD,
-            # "%s install %s --only-deps -c file:/%s/conda-bld yggdrasil" % (
-            #     CONDA_CMD, install_flags, CONDA_PREFIX),
-            "%s install %s --use-local yggdrasil" % (
-                CONDA_CMD, install_flags),
+            "%s install %s -c file:/%s/conda-bld yggdrasil" % (
+                CONDA_CMD, install_flags, CONDA_PREFIX),
+            # "%s install %s --use-local yggdrasil" % (
+            #     CONDA_CMD, install_flags),
             # "%s install %s --update-deps -c file:/%s/conda-bld yggdrasil" % (
             #     CONDA_CMD, install_flags, CONDA_PREFIX),
             # Install & then update
