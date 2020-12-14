fc64541c6a1cdf191a4caa26a7992bb53bc4c8d4
Meagan Lang <langmm.astro@gmail.com>
2020-12-14 15:40:06 -0800

    Wrap print statement in python version of transformation example so that encoding error is caught

--- yggdrasil/examples/transforms/src/src.py
+++ yggdrasil/examples/transforms/src/src.py
@@ -1,9 +1,10 @@
 import pprint
+from yggdrasil.tools import print_encoded
 from yggdrasil.examples.tests.test_transforms import TestExampleTransforms
 
 
 def src():
     test_data = TestExampleTransforms.get_test_data()
     print('SENDING')
-    pprint.pprint(test_data)
+    print_encoded(pprint.pformat(test_data))
     return test_data
