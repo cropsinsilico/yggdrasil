4440546f3c2ef161e1c63c9300ffd39c35d2d7e8
Meagan Lang <langmm.astro@gmail.com>
2020-12-15 14:36:00 -0800

    Default to linking against the static version of the Python C library on <3.8 or windows, but the shared version on >= 3.8.

--- yggdrasil/drivers/CModelDriver.py
+++ yggdrasil/drivers/CModelDriver.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 import warnings
 import copy
 import shutil
@@ -459,7 +460,7 @@ if (_python_inc is None) or (not os.path.isfile(_python_inc)):  # pragma: no cov
 else:
     _python_inc = os.path.dirname(_python_inc)
 try:
-    if platform._is_win:  # pragma: windows
+    if platform._is_win or (sys.version_info[:2] < (3, 8)):  # pragma: windows
         libtype_order = ['static', 'shared']
     else:
         libtype_order = ['shared', 'static']
