bbcea124aed810b789eabfa9e45831a53167aa72
Meagan Lang <langmm.astro@gmail.com>
2020-12-21 13:59:26 -0800

    Fix some legacy code that had not been updated to use commtype instead of comm

--- yggdrasil/communication/tests/test_RMQComm.py
+++ yggdrasil/communication/tests/test_RMQComm.py
@@ -41,5 +41,6 @@ def test_running():
 @unittest.skipIf(_rmq_installed, "RMQ Server running")
 def test_not_running():
     r"""Test raise of an error if a RMQ server is not running."""
-    comm_kwargs = dict(comm='RMQComm', direction='send', reverse_names=True)
+    comm_kwargs = dict(commtype='RMQComm', direction='send',
+                       reverse_names=True)
     assert_raises(RuntimeError, new_comm, 'test', **comm_kwargs)
