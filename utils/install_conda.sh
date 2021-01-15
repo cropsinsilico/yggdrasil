227270b428af9a7ff13f977ae47a9ee84364ad61
Meagan Lang <langmm.astro@gmail.com>
2021-01-15 14:03:32 -0800

    Revert to not using script to avoid handling interaction w/ install script

--- utils/install_conda.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Installing Python using conda on $1...;
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-$1-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $2
-export PATH="$2/bin:$PATH"
-hash -r
