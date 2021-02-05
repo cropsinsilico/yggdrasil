d1ba2f32d8581f1a257afaed3c4384df9f7bbe88
Meagan Lang <langmm.astro@gmail.com>
2021-02-04 21:08:08 -0800

    Only run vcpkg_dir config test on windows when vcpkg_dir is already set

--- yggdrasil/tests/test_config.py
+++ yggdrasil/tests/test_config.py
@@ -27,10 +27,12 @@ def test_update_language_config():
     cfg_copy = '_copy'.join(os.path.splitext(cfg_orig))
     shutil.copy2(cfg_orig, cfg_copy)
     try:
-        config.update_language_config(
-            'c', lang_kwargs={'c': {
-                'vcpkg_dir': config.ygg_cfg_usr.get(
-                    'c', 'vcpkg_dir', default=os.getcwd())}})
+        if config.ygg_cfg_usr.has_option('c', 'vcpkg_dir'):
+            config.update_language_config(
+                'c', lang_kwargs={'c': {
+                    'vcpkg_dir': config.ygg_cfg_usr.get(
+                        'c', 'vcpkg_dir')}})
+            # , default=os.getcwd())}})
         if len(languages) > 0:
             config.ygg_cfg_usr.remove_section(languages[0])
             config.update_language_config(
