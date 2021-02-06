71441fc8ce614cd9fc8aec03d380c48dee46d144
Meagan Lang <langmm.astro@gmail.com>
2021-02-05 17:47:01 -0800

    Add debug flags

--- yggdrasil/drivers/CModelDriver.py
+++ yggdrasil/drivers/CModelDriver.py
@@ -404,8 +404,10 @@ class ClangLinker(LDLinker):
         out = cls.call(cls.version_flags, skip_flags=True, allow_error=True)
         regex = r'clang version (?P<version>\d+)\.\d+\.\d+'
         match = re.search(regex, out)
+        cls.info('CLANG VERSION = %s' % out)
         if (match is not None) and (int(match.group('version')) >= 10):
             ld_version = LDLinker.tool_version()
+            cls.info('LDVERSION = %s' % ld_version)
             if float(ld_version.split('.')[0]) < 520:
                 kwargs['linker-version'] = ld_version
         out = super(ClangLinker, cls).get_flags(*args, **kwargs)
