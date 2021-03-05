617a4c7c9d99a11d7bfd9b7e95dc2565af067d90
Meagan Lang <langmm.astro@gmail.com>
2021-03-04 20:17:49 -0800

    Remove duplicate of test that was used to debug non-error

--- yggdrasil/drivers/tests/test_CompiledModelDriver.py
+++ yggdrasil/drivers/tests/test_CompiledModelDriver.py
@@ -138,8 +138,6 @@ class TestDummyCompiler(TestCompilationTool):
         out = 'test123'
         assert(not shutil.which(self.import_cls.toolname))
         assert(not (os.path.isfile(out) or os.path.isdir(out)))
-        if os.environ.get('USEVIRTUALENV', '0') == '1':
-            self.import_cls.call('args', out=out, dont_link=True)
         self.assert_raises(RuntimeError, self.import_cls.call, 'args',
                            out=out, dont_link=True)
 
