c6ffb66b44dc9cf9bba93bb266cb8107ee883bd0
langmm <cfh5058@gmail.com>
2021-05-18 18:41:20 +0000

    Deploying to gh-pages from  @ 5edbc81ae63ff26544b3bd403b5c2648c39ac7f2 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.6.0+5.g622c9ac0',
+    VERSION: '1.6.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
