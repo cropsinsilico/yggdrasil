ae7755711e8205adea669f532dfca5f85e7b7d75
Meagan Lang <langmm.astro@gmail.com>
2021-06-04 20:56:49 -0700

    rebuilt docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.6.3+3.g94d79515e.dirty',
+    VERSION: 'v1.6.3+3.ge84f31dd0.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
