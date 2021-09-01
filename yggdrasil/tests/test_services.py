1eb06c99bab9e452d45408fa1dd5aa1cd0ffc1e6
Meagan Lang <langmm.astro@gmail.com>
2021-09-01 16:50:43 -0700

    Re-enable test that uses heroku service app

--- yggdrasil/tests/test_services.py
+++ yggdrasil/tests/test_services.py
@@ -103,7 +103,6 @@ def _make_ids(ids):
     return ','.join([str(x) for x in ids])
 
 
-@pytest.mark.skip
 def test_call_integration_remote():
     r"""Test with remote integration service."""
     name = 'photosynthesis'
