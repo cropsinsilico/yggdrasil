f1dc53a127534de88eabcfad1b19778f9ec24d1c
Meagan Lang <langmm.astro@gmail.com>
2021-09-29 19:00:30 -0700

    Try allowing double import

--- yggdrasil/components.py
+++ yggdrasil/components.py
@@ -56,7 +56,6 @@ class ClassRegistry(OrderedDict):
         r"""Import all classes in the same directory."""
         if self._imported:
             return
-        self._imported = True
         print('ClassRegistry import_classes', self._module,
               glob.glob(os.path.join(self._directory, '*.py')))
         for x in glob.glob(os.path.join(self._directory, '*.py')):
@@ -64,6 +63,7 @@ class ClassRegistry(OrderedDict):
             if not mod.startswith('__'):
                 print('ClassRegistry importing', mod)
                 importlib.import_module(self._module + '.%s' % mod)
+        self._imported = True
         if self._import_function is not None:
             self._import_function()
 
