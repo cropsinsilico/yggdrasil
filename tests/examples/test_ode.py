8a6f70a119d007c78760dec445931b4718f6d4da
Meagan Lang <langmm.astro@gmail.com>
2021-12-02 11:29:19 -0800

    Move file checking for ODE models into the testing options so that differences within a tolerance are allow (different behavior between runs, probably due to a dependency update)

--- tests/examples/test_ode.py
+++ tests/examples/test_ode.py
@@ -1,9 +1,7 @@
 import pytest
 import shutil
 import os
-import numpy as np
 import yaml as pyyaml
-from yggdrasil import serialize
 from tests.examples import TestExample as base_class
 
 
@@ -25,40 +23,3 @@ class TestODE(base_class):
         pyyaml.dump(contents, open(yaml, 'w'))
         yield
         shutil.move(oldyaml, yaml)
-
-    @pytest.fixture(scope="class")
-    def read_file(self):
-        r"""Read a file."""
-        def read_file_w(fname):
-            with open(fname, 'rb') as fd:
-                return serialize.table_to_array(fd.read(), comment='#')
-        return read_file_w
-    
-    @pytest.fixture(scope="class")
-    def check_file_size(self, wait_on_function):
-        r"""Check that file is the correct size.
-
-        Args:
-            fname (str): Full path to the file that should be checked.
-            fsize (int): Size that the file should be in bytes.
-            timeout (float, optional): Time that should be waited when checking
-                the file size. Defaults to 2.
-
-        """
-        def check_file_size_w(fname, fsize, timeout=2):
-            pass
-        return check_file_size_w
-
-    @pytest.fixture(scope="class")
-    def check_file_contents(self, read_file):
-        r"""Check that the contents of a file are correct.
-
-        Args:
-            fname (str): Full path to the file that should be checked.
-            result (str): Contents of the file.
-
-        """
-        def check_file_contents_w(fname, result):
-            ocont = read_file(fname)
-            np.testing.assert_allclose(ocont, result, rtol=1e-5, atol=1e-8)
-        return check_file_contents_w
--- yggdrasil/examples/ode1/__init__.py
+++ yggdrasil/examples/ode1/__init__.py
@@ -1,10 +1,20 @@
 import os
+import numpy as np
+from yggdrasil import serialize
 
 
 def get_testing_options():
     r"""Get testing parameters for this example."""
+    def read_file(fname):
+        with open(fname, 'rb') as fd:
+            return serialize.table_to_array(fd.read(), comment='#')
+        
+    def compare_results(a, b):
+        np.testing.assert_allclose(a, b, rtol=1e-5, atol=1e-8)
     return dict(
         input_files='input.txt',
+        read_file=read_file,
+        compare_results=compare_results,
         expected_output_files=os.path.join('Output', 'expected.txt'),
         output_dir=None,
         output_files=['output.txt'])
