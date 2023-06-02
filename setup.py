5d2dc282cb30b1e1d42964d206d46a6ae6251f0c
Meagan Lang <langmm.astro@gmail.com>
2023-06-02 10:54:43 -0400

    Parse flag to force version used in relase to the last tag

--- setup.py
+++ setup.py
@@ -11,10 +11,16 @@ sys.path.insert(0, ROOT_PATH)
 import versioneer  # noqa: E402
 ygg_ver = versioneer.get_version()
 
-
 print(f"In setup.py: sys.argv={sys.argv}, PYRJ_PATH={PYRJ_PATH}")
 logging.critical(
     f"In setup.py: sys.argv={sys.argv}, PYRJ_PATH={PYRJ_PATH}")
+
+
+# Clean up version
+if '--force-clean-version' in sys.argv:
+    sys.argv.remove('--force-clean-version')
+    if '+' in ygg_ver:
+        ygg_ver = ygg_ver.split('+')[0]
 
 
 # Get extension options for the vendored python-rapidjson
