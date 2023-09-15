212ea35826e9ccc8a2ec469f8fb996748b0dafbf
Meagan Lang <langmm.astro@gmail.com>
2023-09-15 11:41:59 -0400

    Added specialized header that can be include outside of the main source file for an model (e.g. in the case of OSR where there is more than one source file)

---
+++ yggdrasil/languages/C/YggLibrary.h
@@ -0,0 +1,7 @@
+#ifndef YGGLIBRARY_H_
+#define YGGLIBRARY_H_
+
+#define YGGINTERFACE_OUTSIDE_MAIN
+#include "YggInterface.h"
+
+#endif // YGGLIBRARY_H_
---
+++ yggdrasil/languages/CPP/YggLibrary.hpp
@@ -0,0 +1,7 @@
+#ifndef YGGLIBRARY_HPP_
+#define YGGLIBRARY_HPP_
+
+#define YGGINTERFACE_OUTSIDE_MAIN
+#include "YggInterface.hpp"
+
+#endif // YGGLIBRARY_HPP_
