6f151ad1f3f80341ddc0f23888313b9c9647a72d
Meagan Lang <langmm.astro@gmail.com>
2023-09-27 17:35:08 -0400

    Fix version comparison to exclude example when incorrect sympy installed

--- yggdrasil/examples/ode4/__init__.py
+++ yggdrasil/examples/ode4/__init__.py
@@ -9,7 +9,7 @@ def get_testing_options():
         import sympy
         sympy_ver = version.parse(sympy.__version__)
         # This example hangs indefinitely for sympy 1.10 and 1.11
-        if version.parse("1.10") >= sympy_ver < version.parse("1.12"):
+        if version.parse("1.10") <= sympy_ver < version.parse("1.12"):
             out['skip'] = "sympy >= 1.12 or < 1.10 is required for this example"
     except ImportError:
         pass
