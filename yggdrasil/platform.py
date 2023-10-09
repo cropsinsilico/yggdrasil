daa52ca63673aa5bf6c592435896642d1aad9e78
Meagan Lang <langmm.astro@gmail.com>
2023-10-09 15:45:04 -0500

    Restore newline constants

--- yggdrasil/platform.py
+++ yggdrasil/platform.py
@@ -9,9 +9,12 @@ _is_win = (sys.platform in ['win32', 'cygwin'])
 _supported_platforms = ['Windows', 'MacOS', 'Linux']
 
 if _is_win:  # pragma: windows
+    _newline = b'\r\n'
     _platform = 'Windows'
 else:
+    _newline = b'\n'
     if _is_mac:
         _platform = 'MacOS'
     elif _is_linux:
         _platform = 'Linux'
+_newline_str = _newline.decode("utf-8")
