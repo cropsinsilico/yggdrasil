be43ca3f67e773ec3724a6e55d05831c63834e8d
Meagan Lang <langmm.astro@gmail.com>
2023-10-09 13:11:32 -0500

    Remove unused constant _newline and _newline_str and update callback example to write output as bytes for platform compat

--- yggdrasil/examples/callback/src/callback.py
+++ yggdrasil/examples/callback/src/callback.py
@@ -1,5 +1,5 @@
 import os
-from yggdrasil import units, platform
+from yggdrasil import units, constants
 fname = os.path.join(os.path.dirname(os.path.dirname(__file__)),
                      'outputCallback.txt')
 if os.path.isfile(fname):
@@ -7,5 +7,6 @@ if os.path.isfile(fname):
 
 
 def callback_function(msg):
-    with open(fname, 'a') as fd:
-        fd.write(str(units.get_data(msg.args)) + platform._newline_str)
+    with open(fname, 'ab') as fd:
+        fd.write((str(units.get_data(msg.args))
+                  + constants.DEFAULT_NEWLINE_STR).encode("utf-8"))
